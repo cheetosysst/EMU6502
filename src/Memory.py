f41bf89313f01440251924b8bb9b983aac3339d8
Thect <thect@protonmail.com>
2021-06-28 14:51:42 +0800

    memory load and dump

--- src/Memory.py
+++ src/Memory.py
@@ -13,6 +13,16 @@ class memory:
 		self.Data = [0] * self._MEMORY_SIZE_MAX
 		pass
 
-	def loadBinary(self):
+	def loadBinary(self, path):
 		"Load Binary File (not implemented yet)"
+		with open("src/65C02_extended_opcodes_test.bin", "rb") as f, mmap(f.fileno(), 0, access=ACCESS_READ) as s:
+			for i in range(0xffff):
+				self.Data[i] = s[i]
 		pass
+
+	def memoryDump(self, path):
+		"Dump memory to a file"
+		file = open(path, "wb")
+		for i in range(0xffff):
+			file.write(self.Data[i])
+		file.close()
