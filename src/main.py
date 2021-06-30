6da899b6a030824a7a1661b388d5c1cdc116cd2f
Thect <thect@protonmail.com>
2021-06-30 15:34:21 +0800

    ADC

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
 from cpu import cpu
+from memory import memory
 
 CPU = cpu()
 
 # Immediate
-CPU._memory.Data[0x0000] = 0xa9
-CPU._memory.Data[0x0001] = 0xaa
+CPU._memory.Data[0x0000] = 0x6d
+CPU._memory.Data[0x0001] = 0xbb
+CPU._memory.Data[0x0002] = 0xaa
+CPU._memory.Data[0xaabb] = 0xff
+CPU._memory.memoryDump("./dump")
+CPU._Acc = 0x12
 CPU.execute()
 print("acc = {}".format(hex(CPU._Acc)))
+print("carry = {}".format(hex(CPU._PS_c)))
 
 
